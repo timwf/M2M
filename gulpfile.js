729363fc3075496dd05728d699859e9a50fe6d84
Bogdan Skuratov <bogdanskuratov@Bogdans-MacBook-Pro.local>
2020-03-03 13:30:20 +0200

    Removed gulp images tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const browserSync = require('browser-sync').create();
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const rename = require('gulp-rename');
-const svgmin = require('gulp-svgmin');
-const svgstore = require('gulp-svgstore');
-const cheerio = require('gulp-cheerio');
-const webp = require('gulp-webp');
-const imagemin = require('gulp-imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminMozjpeg = require('imagemin-mozjpeg');
+// const svgmin = require('gulp-svgmin');
+// const svgstore = require('gulp-svgstore');
+// const cheerio = require('gulp-cheerio');
+// const webp = require('gulp-webp');
+// const imagemin = require('gulp-imagemin');
+// const imageminPngquant = require('imagemin-pngquant');
+// const imageminMozjpeg = require('imagemin-mozjpeg');
 const cssnano = require('cssnano');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
@@ -71,50 +71,50 @@ gulp.task('libs', () => {
 gulp.task('watch', gulp.series('sass', 'libs', 'scripts', 'browserSync'));
 
 // Optimization Tasks
-gulp.task('webp', () =>
-  gulp
-    .src('./img/hero_*.{jpg,png}')
-    .pipe(webp())
-    .pipe(gulp.dest('./img/'))
-);
+// gulp.task('webp', () =>
+//   gulp
+//     .src('./img/hero_*.{jpg,png}')
+//     .pipe(webp())
+//     .pipe(gulp.dest('./img/'))
+// );
 
-gulp.task('imagemin', () =>
-  gulp
-    .src('img/*.{jpg,png}')
-    .pipe(
-      imagemin(
-        [
-          (imageminPngquant({
-            speed: 1,
-            quality: 98 // lossy settings
-          }),
-          imageminMozjpeg({
-            quality: 90
-          }))
-        ],
-        {
-          verbose: true
-        }
-      )
-    )
-    .pipe(gulp.dest('img/'))
-);
+// gulp.task('imagemin', () =>
+//   gulp
+//     .src('img/*.{jpg,png}')
+//     .pipe(
+//       imagemin(
+//         [
+//           (imageminPngquant({
+//             speed: 1,
+//             quality: 98 // lossy settings
+//           }),
+//           imageminMozjpeg({
+//             quality: 90
+//           }))
+//         ],
+//         {
+//           verbose: true
+//         }
+//       )
+//     )
+//     .pipe(gulp.dest('img/'))
+// );
 
 // Sprites
-gulp.task('sprite', () => {
-  return gulp
-    .src('./img/icon-*.svg')
-    .pipe(svgmin())
-    .pipe(svgstore({ inlineSvg: true }))
-    .pipe(
-      cheerio({
-        run($) {
-          $('[fill]').removeAttr('fill');
-          $('svg').attr('style', 'display:none');
-        },
-        parserOptions: { xmlMode: true }
-      })
-    )
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('./img/'));
-});
+// gulp.task('sprite', () => {
+//   return gulp
+//     .src('./img/icon-*.svg')
+//     .pipe(svgmin())
+//     .pipe(svgstore({ inlineSvg: true }))
+//     .pipe(
+//       cheerio({
+//         run($) {
+//           $('[fill]').removeAttr('fill');
+//           $('svg').attr('style', 'display:none');
+//         },
+//         parserOptions: { xmlMode: true }
+//       })
+//     )
+//     .pipe(rename('sprite.svg'))
+//     .pipe(gulp.dest('./img/'));
+// });
